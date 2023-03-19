1e2f68542637e62d1730dd915c4b873bb9b2a654
Elijah Pereira <vahpourvisuals@gmail.com>
2023-03-19 03:26:54 -0400

    redesigned work ui

--- script.js
+++ script.js
@@ -4,47 +4,31 @@ const toggleNav = () => {
   document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
 }
 
-const ele = document.getElementById('nav-links');
-ele.scrollTop = 100;
-ele.scrollLeft = 150
+let slideIndex = 1;
+showSlides(slideIndex);
 
-let pos = { top: 0, left: 0, x: 0, y: 0 };
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-const mouseDownHandler = function (e) {
-    pos = {
-        // The current scroll
-        left: ele.scrollLeft,
-        top: ele.scrollTop,
-        // Get the current mouse position
-        x: e.clientX,
-        y: e.clientY,
-    };
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseup', mouseUpHandler);
-};
-
-const mouseMoveHandler = function (e) {
-  // How far the mouse has been moved
-  const dx = e.clientX - pos.x;
-  const dy = e.clientY - pos.y;
-
-  // Scroll the element
-  ele.scrollTop = pos.top - dy;
-  ele.scrollLeft = pos.left - dx;
-};
-
-const mouseDownHandler = function(e) {
-  // Change the cursor and prevent user from selecting the text
-  ele.style.cursor = 'grabbing';
-  ele.style.userSelect = 'none';
-  ...
-};
-
-const mouseUpHandler = function () {
-  document.removeEventListener('mousemove', mouseMoveHandler);
-  document.removeEventListener('mouseup', mouseUpHandler);
-
-  ele.style.cursor = 'grab';
-  ele.style.removeProperty('user-select');
-};
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
